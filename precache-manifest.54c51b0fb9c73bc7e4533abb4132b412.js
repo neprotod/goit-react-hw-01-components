beefffe3ff3e2faede591d3b7fe8f3c30d8ab5b2
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-12-11 20:32:34 +0200

    Updates

---
+++ precache-manifest.54c51b0fb9c73bc7e4533abb4132b412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f39dd6f5c832d9145d2c0070485935",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fec82d8a9a8d935289f1",
+    "url": "/goit-react-hw-01-components/static/css/main.741146b7.chunk.css"
+  },
+  {
+    "revision": "77736cbb8e048106443c",
+    "url": "/goit-react-hw-01-components/static/js/2.532a848b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.532a848b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fec82d8a9a8d935289f1",
+    "url": "/goit-react-hw-01-components/static/js/main.e68d442b.chunk.js"
+  },
+  {
+    "revision": "351ec24c9bd9afb33e31",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.b5c70b18.js"
+  }
+]);
